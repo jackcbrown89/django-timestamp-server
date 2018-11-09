5774812b2f243c7dcf03af1a533c8b5871fb0682
Jack Brown <jackcbrown89@gmail.com>
2018-11-09 12:19:31 -0500

    django eb config

--- timestampserver/settings.py
+++ timestampserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(6y5wtmz5svl6_7y)y97(v*#j1z-4%6*%r+=qrb2^bv@dxp7j8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "django-tss.wyrjngu4gj.us-east-2.elasticbeanstalk.com"]
 
 
 # Application definition
